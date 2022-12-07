09447b67354ae97858908ff88c01db4a6d6c55d4
Daria Kulkova <d.kulkovva@icloud.com>
2022-12-07 22:04:41 +0300

    finished project

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -8,12 +8,53 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var redLight: UIView!
+    @IBOutlet var yellowLight: UIView!
+    @IBOutlet var greenLight: UIView!
+    
+    @IBOutlet var signalSwitchButton: UIButton!
+    
+    private let lightOff: CGFloat = 0.3
+    private let lightOn: CGFloat = 1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redLight.alpha = lightOff
+        yellowLight.alpha = lightOff
+        greenLight.alpha = lightOff
     }
-
-
+    
+    @IBAction func signalSwitchButtonPressed() {
+        if signalSwitchButton.currentTitle == "START" {
+            signalSwitchButton.setTitle("NEXT", for: .normal)
+            
+            redLight.alpha = lightOn
+            
+        } else {
+            if redLight.alpha == lightOn {
+                redLight.alpha = lightOff
+                yellowLight.alpha = lightOn
+            } else if yellowLight.alpha == lightOn {
+                yellowLight.alpha = lightOff
+                greenLight.alpha = lightOn
+            } else if greenLight.alpha == lightOn {
+                greenLight.alpha = lightOff
+                redLight.alpha = lightOn
+            }
+        }
+    }
+    override func viewWillLayoutSubviews() {
+        redLight.layer.cornerRadius = redLight.frame.width / 2
+        yellowLight.layer.cornerRadius = yellowLight.frame.width / 2
+        greenLight.layer.cornerRadius = greenLight.frame.width / 2
+        
+        signalSwitchButton.layer.cornerRadius = 10
+    }
 }
+
+
+
+
 
